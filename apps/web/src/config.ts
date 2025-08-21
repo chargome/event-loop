4d9e506b56255038b5633e90555dd038677ad057
Charly Gomez <charly.gomez@sentry.io>
2025-08-21 14:08:33 +0200

    fix api url

--- apps/web/src/config.ts
+++ apps/web/src/config.ts
@@ -1,5 +1,5 @@
 export const config = {
   CLERK_PUBLISHABLE_KEY:
     "pk_test_aWRlYWwtYWtpdGEtMzUuY2xlcmsuYWNjb3VudHMuZGV2JA",
-  API_URL: "https://event-loop-api.charlygomez.workers.dev",
+  API_URL: "https://event-loop-api.charly-gomez.workers.dev",
 } as const;
